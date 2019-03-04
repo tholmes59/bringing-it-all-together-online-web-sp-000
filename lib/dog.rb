7654bf9b3833de7b9dd5c3a6d809c82748b093e7
Tom Holmes <tholmes59@gmail.com>
2019-03-04 21:50:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,7 +58,7 @@ class Dog
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2])
+    Dog.new(result[0], result[1], result[2])
   end
   
   def update
