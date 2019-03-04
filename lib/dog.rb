ce38f8e0c7f537cb722f9917dab378a3b3967664
Tom Holmes <tholmes59@gmail.com>
2019-03-04 21:30:46 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  
+  attr_accessor :id, :name, :breed 
+  
+  def initialize(id = nil, name, breed)
+    @id = id
+    @name = name
+    @breed = breed
+  end 
+  
+  def self.create_table
+    sql = <<- SQL 
+      CREATE TABLE 
+    SQL 
+  end 
+  
+end
