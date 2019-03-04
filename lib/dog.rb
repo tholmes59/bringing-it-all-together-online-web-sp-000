49dff721ab8714325e55817b637d594d9b65d111
Tom Holmes <tholmes59@gmail.com>
2019-03-04 21:52:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,8 +50,8 @@ class Dog
   
   def self.new_from_db(row)
     attributes = {
-    id => row[0]
-    name => row[1]
+    id => row[0],
+    name => row[1],
     breed => row[2]
     }
     self.new(attributes)
