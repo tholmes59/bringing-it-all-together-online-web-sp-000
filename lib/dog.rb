a872065e550ddc877dc59c128293a12349c69720
Tom Holmes <tholmes59@gmail.com>
2019-03-04 22:05:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,7 +51,7 @@ class Dog
     end.first
   end
   
-  def self.find_or_create_by(attributes)
+  def self.find_or_create_by(:name, :breed)
      sql = <<-SQL
       SELECT * FROM dogs
       WHERE name = ? AND breed = ?
@@ -62,7 +62,7 @@ class Dog
       if dog
         new_dog = self.new_from_db(dog)
       else
-        new_dog = self.create(attributes)
+        new_dog = self.create({:name => name, :breed => breed})
       end
       new_dog
   end 
