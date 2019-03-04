05b371a5442c9d015594118016016f682d8b2878
Tom Holmes <tholmes59@gmail.com>
2019-03-04 21:50:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,8 +51,8 @@ class Dog
   def self.new_from_db(row)
     id = row[0]
     name = row[1]
-    grade = row[2]
-    self.new(id, name, grade)
+    breed = row[2]
+    self.new(attributes)
   end
   
   def self.find_by_name(name)
