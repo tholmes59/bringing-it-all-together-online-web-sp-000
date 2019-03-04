a930d1a946d2e6e5b6db3b7d3f69c1a797dfefa2
Tom Holmes <tholmes59@gmail.com>
2019-03-04 22:03:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,7 +60,7 @@ class Dog
       dog = DB[:conn].execute(sql, name, breed).first
 
       if dog
-        new_dog = self.new_from_db(row)
+        new_dog = self.new_from_db(dog)
       else
         new_dog = self.create({:name => name, :breed => breed})
       end
