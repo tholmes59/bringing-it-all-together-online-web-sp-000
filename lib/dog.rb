607fc57062ef3502dff0dcb5476062f63708be4f
Tom Holmes <tholmes59@gmail.com>
2019-03-04 21:35:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,4 +28,13 @@ class Dog
     DB[:conn].execute(sql)
   end 
   
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed) 
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+  end
+  
 end
